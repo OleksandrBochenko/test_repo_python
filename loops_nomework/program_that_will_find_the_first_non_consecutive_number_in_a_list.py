3b70287251e1c62a413a758da7855bc22c0cbee2
Oleksandr_Bochenko <palekseenko1990@gmail.com>
2024-03-29 20:10:06 +0100

    add changes

--- loops_nomework/program_that_will_find_the_first_non_consecutive_number_in_a_list.py
+++ loops_nomework/program_that_will_find_the_first_non_consecutive_number_in_a_list.py
@@ -1,7 +1,7 @@
 my_list = [1, 2, 3, 4, 6, 7, 8]
-filtered = []
 for element in range(0, len(my_list) - 1):
     if my_list[element] + 1 != my_list[element + 1]:
-         print(my_list[element + 1])
-         break
-
+        print(f'{my_list[element + 1]} is not consecutive')
+        break
+else:
+    print(f'{my_list} is consecutive')
