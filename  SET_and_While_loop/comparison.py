228fa7834a9e2f33b4a723fee59f5f6233e9d717
Oleksandr_Bochenko <palekseenko1990@gmail.com>
2024-04-01 16:17:53 +0200

    homework set and while

---
+++  SET_and_While_loop/comparison.py
@@ -0,0 +1,4 @@
+set_1 = {1, 2, 3, 4, 5, 6}
+set_2 = {4, 6, 7, 8, 9, 10}
+set_3 = set_1.difference(set_2)
+print(set_3)
---
+++  SET_and_While_loop/sum_of_numbers.py
@@ -0,0 +1,6 @@
+elements = [1, 5, 68, 10]
+ind = 0
+for element in elements:
+    ind = ind + element
+print(f'sum of elements is {ind}')
+
